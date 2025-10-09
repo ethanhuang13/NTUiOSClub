32644ab8ea087f057c0291b6d5359431e8317824
Ethan Huang <ethanhuang13@icloud.com>
2025-10-09 11:10:59 +0800

    Initial Commit

---
+++ NTUiOSClub/NTUiOSClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  NTUiOSClubApp.swift
+//  NTUiOSClub
+//
+//  Created by Ethan Huang on 2025/10/9.
+//
+
+import SwiftUI
+
+@main
+struct NTUiOSClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
