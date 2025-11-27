f912652f4bb69afe105194ae00ad1aef3786d510
Ethan Huang <ethanhuang13@icloud.com>
2025-11-27 21:43:55 +0800

    Replace with AnyLanguageModel

--- NTUiOSClub/Foundation Models/FoodView.swift
+++ NTUiOSClub/Foundation Models/FoodView.swift
@@ -5,7 +5,7 @@
 //  Created by ethanhuang on 2025/11/27.
 //
 
-import FoundationModels
+import AnyLanguageModel
 import SwiftUI
 
 // MARK: 把 Foundation Models 放進 SwiftUI
@@ -29,16 +29,20 @@ struct FoodView: View {
 
   func startStreamResponse() {
     Task {
+      let model = OpenAILanguageModel(
+        baseURL: URL(string: "http://127.0.0.1:1234/v1")!,
+        apiKey: "",
+        model: "openai/gpt-oss-20b"
+      )
+
       let session = LanguageModelSession(
-        model: SystemLanguageModel.default,
+        model: model,
         instructions: "這是一個關於美食的對話"
       )
 
       let stream = session.streamResponse(to: "午餐要吃什麼？我在台大")
       for try await partialResponse in stream {
-        withAnimation {
-          response = partialResponse.content
-        }
+        response = partialResponse.content
       }
     }
   }
